gewaltig/cython-neuron

commit c4b2ca1f4ab3fcab03886eab73928704ae5fe984

* svn merge -r 9236:9261 nest2 nest2.10kproject  * Some tests still fail. Will look into them soon...
git-svn-id: https://www.nest-initiative.uni-freiburg.de/nest/branches/10kproject@9265 4fa6ec7b-3718-0410-b4d6-90ca9082ef84

nestkernel/scheduler.cpp
@@ -138,6 +138,7 @@ void nest::Scheduler::init_()
 	       "Error initializing condition variable ready_");
     throw PthreadException(status);
   }
+
 #else
 #ifndef _OPENMP
   if (n_threads_ > 1)
@@ -320,14 +321,17 @@ void nest::Scheduler::simulate(Time const & t)
     net_.message(SLIInterpreter::M_ERROR, "Scheduler::simulate", 
 		 String::compose("Simulation time must be >= %1 ms (one time step).", 
 				 Time::get_resolution().get_ms()));
-    return;
+    throw KernelException();
   }
 
   // Check for synchronicity of global rngs over processes
   if(Communicator::get_num_processes() > 1)
     if (!Communicator::grng_synchrony(grng_->ulrand(100000)))
-      net_.message(SLIInterpreter::M_WARNING, "Scheduler::simulate", 
-		   "Global Random Number Generators are not synchronized.");
+      {
+        net_.message(SLIInterpreter::M_ERROR, "Scheduler::simulate",
+	  	     "Global Random Number Generators are not synchronized prior to simulation.");
+        throw KernelException();
+      }
 
   // As default, we have to place the iterator at the
   // leftmost node of the tree.
@@ -361,7 +365,7 @@ void nest::Scheduler::simulate(Time const & t)
        << (Time::max()-clock_).get_ms() << " of " << t.get_ms() << " ms.";
       net_.message(SLIInterpreter::M_ERROR, "Scheduler::simulate", os.str().c_str());
       net_.message(SLIInterpreter::M_ERROR, "Scheduler::simulate", "Please reset network clock first!");
-      return;
+      throw KernelException();
     }
   }
   else
@@ -371,7 +375,7 @@ void nest::Scheduler::simulate(Time const & t)
        << "(T_max = " << Time::max().get_ms() << "ms).";
     net_.message(SLIInterpreter::M_ERROR, "Scheduler::simulate", os.str().c_str());
     net_.message(SLIInterpreter::M_ERROR, "Scheduler::simulate", "Please use a shorter simulation time!");
-    return;
+    throw KernelException();
   }
   to_do_ += t.get_steps();
   to_do_total_ = to_do_;
@@ -408,10 +412,15 @@ void nest::Scheduler::simulate(Time const & t)
 
   resume();
   simulated_ = true;
-  //debug of modelrangemanager, can be removed once stable (AM 14/2/2010)
-  if (Communicator::get_rank() == 0)
-    net_.print_model_ranges();
 
+  // Check for synchronicity of global rngs over processes
+  if(Communicator::get_num_processes() > 1)
+    if (!Communicator::grng_synchrony(grng_->ulrand(100000)))
+    {
+      net_.message(SLIInterpreter::M_ERROR, "Scheduler::simulate",
+                   "Global Random Number Generators are not synchronized after simulation.");
+      throw KernelException();
+    }
 }
 
 void nest::Scheduler::resume()
@@ -468,7 +477,7 @@ void nest::Scheduler::resume()
   }
 
   if (n_threads_ == 1)
-    serial_update(); 
+    serial_update();
   else
   {
 #ifdef HAVE_PTHREADS
@@ -490,84 +499,6 @@ void nest::Scheduler::resume()
     serial_update();
 #endif
 #endif
-  }
-  simulating_ = false;
-  finalize_nodes();
-
-  if (print_time_)
-    std::cout << std::endl;
-
-  Communicator::synchronize();
-
-  if(terminate_)
-  {
-    net_.message(SLIInterpreter::M_ERROR, "Scheduler::resume", "Exiting on error or user signal.");
-    net_.message(SLIInterpreter::M_ERROR, "Scheduler::resume", "Scheduler: Use 'ResumeSimulation' to resume.");
-    
-    if(SLIsignalflag != 0)
-    {
-      SystemSignal signal(SLIsignalflag);
-      SLIsignalflag=0;
-      throw signal;
-    }
-    else
-      throw SimulationError();
-    
-    return;
-  }
-  
-  net_.message(SLIInterpreter::M_INFO, "Scheduler::resume", "Simulation finished.");
-}
-
-/**
- * the single-threaded update is just a single loop over all nodes
- * per time-slice.
- */
-void nest::Scheduler::serial_update()
-{
-  std::vector<Node*>::iterator i;
-
-  do
-  {
-    if (print_time_)
-      gettimeofday(&t_slice_begin_, NULL);
-
-    if ( from_step_ == 0 )  // deliver only at beginning of slice
-    {
-      deliver_events_(0);
-#ifdef HAVE_MUSIC
-      // advance the time of music by one step (min_delay * h) must
-      // be done after deliver_events_() since it calls
-      // music_event_out_proxy::handle(), which hands the spikes over to
-      // MUSIC *before* MUSIC time is advanced
-      if (slice_ > 0)
-        Communicator::advance_music_time(1);
-
-      net_.update_music_event_handlers_(clock_, from_step_, to_step_);
-#endif
-    }
-
-    for (i = nodes_vec_[0].begin(); i != nodes_vec_[0].end(); ++i)
-      update_(*i);
-      
-    if ( static_cast<ulong_t>(to_step_) == min_delay_ ) // gather only at end of slice 
-      gather_events_();
-
-    advance_time_();
-
-    if(SLIsignalflag != 0)
-    {
-      net_.message(SLIInterpreter::M_INFO, "Scheduler::serial_update", "Simulation exiting on user signal.");
-      terminate_=true;
-    }
-
-    if (print_time_)
-    { 
-      gettimeofday(&t_slice_end_, NULL);
-      print_progress_();
-    }
-  } while((to_do_ != 0) && (! terminate_));
-}
 
 void nest::Scheduler::threaded_update_openmp()
 {
@@ -792,7 +723,7 @@ void nest::Scheduler::threaded_update(thread t)
 
 }
 #else
-void nest::Scheduler::threaded_update(thread t)
+void nest::Scheduler::threaded_update(thread)
 {
    net_.message(SLIInterpreter::M_ERROR, "Scheduler::threaded_update", "Multithreading mode not available");
    throw KernelException();
@@ -902,18 +833,27 @@ void nest::Scheduler::set_status(DictionaryDatum const &d)
   if (n_threads_updated)
   {
     if (net_.size() > 1)
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-                 "Nodes already exist. Please call ResetKernel first.");
-    else if (net_.connection_manager_.has_user_prototypes())
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-                 "Custom synapse types already exist. Please call ResetKernel first.");
-    else if ( net_.get_simulated() )  // someone may have simulated empty network
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-                 "The network has already been simulated. Please call ResetKernel first.");
-    else if ((n_threads > 1) && (force_singlethreading_))
       {
 	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-		   "No multithreading available, using single threading");
+		     "Nodes already exist. Please call ResetKernel first.");
+	throw KernelException();
+      }
+    else if (net_.connection_manager_.has_user_prototypes())
+      {
+	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		     "Custom synapse types already exist. Please call ResetKernel first.");
+	throw KernelException();
+      }
+    else if ( net_.get_simulated() )  // someone may have simulated empty network
+      {
+	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		     "The network has already been simulated. Please call ResetKernel first.");
+	throw KernelException();
+      }
+    else if ( (n_threads > 1) && (force_singlethreading_) )
+      {
+	net_.message(SLIInterpreter::M_WARNING, "Scheduler::set_status",
+		     "No multithreading available, using single threading");
 	n_threads_ = 1;
       }
     else
@@ -931,14 +871,23 @@ void nest::Scheduler::set_status(DictionaryDatum const &d)
   if (n_vps_updated)
   {
     if (net_.size() > 1)
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-		   "Nodes already exist. Please call ResetKernel first.");
+      {
+	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		     "Nodes already exist. Please call ResetKernel first.");
+	throw KernelException();
+      }
     else if (net_.connection_manager_.has_user_prototypes())
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-                 "Custom synapse types already exist. Please call ResetKernel first.");
+      {
+	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		     "Custom synapse types already exist. Please call ResetKernel first.");
+	throw KernelException();
+      }
     else if ( net_.get_simulated() )  // someone may have simulated empty network
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-                 "The network has already been simulated. Please call ResetKernel first.");
+      {
+	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		     "The network has already been simulated. Please call ResetKernel first.");
+	throw KernelException();
+      }
     else if  (n_vps % Communicator::get_num_processes() != 0)
     {
       throw BadProperty("Number of virtual processes (threads*processes) must be an integer"
@@ -949,7 +898,7 @@ void nest::Scheduler::set_status(DictionaryDatum const &d)
       n_threads_ = n_vps / Communicator::get_num_processes();
       if ((n_threads > 1) && (force_singlethreading_))
       {
-        net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+        net_.message(SLIInterpreter::M_WARNING, "Scheduler::set_status",
                    "No multithreading available, using single threading");
         n_threads_ = 1;
       }
@@ -973,19 +922,31 @@ void nest::Scheduler::set_status(DictionaryDatum const &d)
   if (tics_per_ms_updated || res_updated)
   {
     if (net_.size() > 1) // root always exists
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-                 "Cannot change time representation after nodes have been created. Please call ResetKernel first.");
+      {
+	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		     "Cannot change time representation after nodes have been created. Please call ResetKernel first.");
+	throw KernelException();
+      }
     else if ( net_.get_simulated() )  // someone may have simulated empty network
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-                 "Cannot change time representation after the network has been simulated. Please call ResetKernel first.");
+      {
+	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		     "Cannot change time representation after the network has been simulated. Please call ResetKernel first.");
+	throw KernelException();
+      }
     else if ( net_.connection_manager_.get_num_connections() != 0 )
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-		 "Cannot change time representation after connections have been created. Please call ResetKernel first.");
+      {
+	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		     "Cannot change time representation after connections have been created. Please call ResetKernel first.");
+	throw KernelException();
+      }
     else if (res_updated && tics_per_ms_updated) // only allow TICS_PER_MS to be changed together with resolution
     {
       if ( resd < 1.0 / tics_per_ms )
-	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-		   "Resolution must be greater than or equal to one tic. Value unchanged.");
+	{
+	  net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		       "Resolution must be greater than or equal to one tic. Value unchanged.");
+	  throw KernelException();
+	}
       else
       {
 	nest::TimeModifier::set_time_representation(tics_per_ms, resd);
@@ -997,8 +958,11 @@ void nest::Scheduler::set_status(DictionaryDatum const &d)
     else if (res_updated) // only resolution changed
     {
       if ( resd < Time::get_ms_per_tic() )
-	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-		   "Resolution must be greater than or equal to one tic. Value unchanged.");
+	{
+	  net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		       "Resolution must be greater than or equal to one tic. Value unchanged.");
+	  throw KernelException();
+	}
       else
       {
 	Time::set_resolution(resd);
@@ -1008,8 +972,11 @@ void nest::Scheduler::set_status(DictionaryDatum const &d)
       }
     }
     else
-      net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
-		 "change of tics_per_step requires simultaneous specification of resolution.");
+      {
+	net_.message(SLIInterpreter::M_ERROR, "Scheduler::set_status",
+		     "change of tics_per_step requires simultaneous specification of resolution.");
+	throw KernelException();
+      }
   }
 
 

developer/pp_psc_delta.cpp
@@ -16,7 +16,7 @@
  *
  */
 
-/* pp_psc_delta is a neuron where the potential jumps on each spike arrival. */
+/* pp_psc_delta is a stochastically spiking neuron where the potential jumps on each spike arrival. */
 
 #include "exceptions.h"
 #include "pp_psc_delta.h"
@@ -60,7 +60,7 @@ nest::pp_psc_delta::Parameters_::Parameters_()
     dead_time_shape_  (    1     ),
     with_reset_       (    1     ),
     tau_sfa_          (  34.0    ),  // ms
-    q_sfa_            (   0.0    ),  // mV, reasonable default is 7 mV
+    q_sfa_            (   0.0    ),  // mV, reasonable default is 7 mV [2]
     c_1_              (   0.0    ),  // Hz / mV
     c_2_              (   1.238  ),  // Hz / mV
     c_3_              (   0.25   ),  // 1.0 / mV
@@ -120,7 +120,7 @@ void nest::pp_psc_delta::Parameters_::set(const DictionaryDatum& d)
     throw BadProperty("Absolute refractory time must not be negative.");
 
   if ( dead_time_shape_ < 1 )
-    throw BadProperty("Shape of the dead time density must not be smaller than 1.");
+    throw BadProperty("Shape of the dead time gamma distribution must not be smaller than 1.");
 
   if ( tau_m_ <= 0 )
     throw BadProperty("All time constants must be strictly positive.");
@@ -221,14 +221,14 @@ void nest::pp_psc_delta::calibrate()
   // should be carried out via objects of class nest::Time. The conversion
   // requires 2 steps:
   //
-  //     1. A time object r is constructed defining  representation of
+  //     1. A time object r is constructed defining the representation of
   //        TauR in tics. This representation is then converted to computation time
   //        steps again by a strategy defined by class nest::Time.
-  //     2. The refractory time in units of steps is read out get_steps(), a member
+  //     2. The refractory time in units of steps is read out by get_steps(), a member
   //        function of class nest::Time.
   //
   // The definition of the refractory period of the pp_psc_delta is consistent
-  // the one of iaf_neuron_ps.
+  // with the one of iaf_neuron_ps.
   //
   // Choosing a TauR that is not an integer multiple of the computation time
   // step h will lead to accurate (up to the resolution h) and self-consistent

developer/pp_psc_delta.h
@@ -41,14 +41,16 @@ namespace nest{
 
      Spikes are generated randomly according to the current value of the
      transfer function which operates on the membrane potential. Spike
-     generation is followed by an optional dead time.
+     generation is followed by an optional dead time. Setting with_reset to 
+     true will reset the membrane potential after each spike.
 
      The transfer function can be chosen to be linear, exponential or both by
      adjusting three parameters:
 
          rate = Rect[ c1 * V' + c2 * exp(c3 * V') ],
 
-     where the effective potential V' = V - Q and Q is the adaptive threshold.
+     where the effective potential V' = V_m - E_sfa and E_sfa is the adaptive 
+     threshold.
 
      By setting c3 = 0, c2 can be used as an offset spike rate for an otherwise
      linear model.
@@ -60,17 +62,21 @@ namespace nest{
 
      Spike generation can then be efficiently simulated by drawing uniform
      numbers. So, even if non-refractory neurons are to be modeled,
-     dead_time = 1e-8 might be the value of choice since it uses a faster
-     implementation than dead_time=0. Only for large spike rates (> 1 spike/h)
-     this will be wrong.
+     dead_time=1e-8 might be the value of choice since it uses a faster
+     random numbers than dead_time=0. Only for large spike rates (> 1 spike/h)
+     this will cause errors.
 
-     The model can optionally include relative refractoriness implemented as
-     an adaptive threshold. If the neuron is spiking too fast, the threshold
-     goes up and the membrane potential can no longer reach it. In the current
-     implementation it jumps by q_sfa when the neuron fires a spike and decays
-     exponentially with the time constant tau_sfa (see [2]).
+     The model can optionally include something which would be called adaptive 
+     threshold in an integrate-and-fire neuron. If the neuron spikes, the 
+     threshold goes up and the membrane potential will take longer to reach it. 
+     Here this is implemented by subtracting the value of the adaptive threshold
+     E_sfa from the membrane potential V_m before passing the potential to the 
+     transfer function. E_sfa jumps by q_sfa when the neuron fires a spike, and 
+     decays exponentially with the time constant tau_sfa after (see [2]).
 
-     This model has been adapted from iaf_psc_delta.
+     This model has been adapted from iaf_psc_delta. The default parameters are
+     set to the mean values in [2], which have been matched to spike-train 
+     recordings.
 
 
      Reference:
@@ -94,10 +100,13 @@ namespace nest{
      dead_time         double - Duration of the dead time in ms.
      dead_time_random  bool   - Should a random dead time be drawn after each spike?
      dead_time_shape   int    - Shape parameter of dead time gamma distribution.
+     t_ref_remaining   double   Remaining dead time at simulation start.
+     with_reset        bool     Should the membrane potential be reset after a spike?
      I_e               double - Constant input current in pA.
      c_1               double - Slope of linear part of transfer function in Hz/mV.
      c_2               double - Prefactor of exponential part of transfer function in Hz.
      c_3               double - Coefficient of exponential non-linearity of transfer function in 1/mV.
+
 
 
      Sends: SpikeEvent

developer/unittests/test_gamma_sup_generator.sli
@@ -1,0 +1,328 @@
+
+/* 
+ *  test_gamma_sup_generator.sli
+ *
+ *  This file is part of NEST
+ *
+ *  Copyright (C) 2004-2011 by
+ *  The NEST Initiative
+ *
+ *  See the file AUTHORS for details.
+ *
+ *  Permission is granted to compile and modify
+ *  this file for non-commercial use.
+ *  See the file LICENSE for details.
+ *  
+ */
+
+ /* BeginDocumentation
+Name: testsuite::test_gamma_sup_generator - sli script for test of gamma_sup_generator output 
+
+Synopsis: (test_gamma_sup_generator) run -> compare spike train statistics with expectations
+
+
+Description:
+ 
+ test_gamma_sup_generator is a collection of tests which require basic 
+ functionality of the generator. It tests
+ 1) if the firing rate of a superposition is close to the preset one.
+ 2) if the coefficient of variation of a superposition agrees with theory
+ 3) if the coefficient of variation of a single process agrees with theory
+ 4) if the spike trains generated for two different targets differ
+
+ All of these tests are based on random number realizations, which is 
+ necessarily so  since the model is stochastic. There is thus a finite 
+ probability of test failure, even if everything is fine. The choice of the 
+ variable err, which is the allowed relative deviation from the reference value, 
+ can be used to make the test more or less strict. Increasing T inside the test 
+ functions can also help to get more reliable statistics and a reduced 
+ probability of false alarms.
+ 
+ The values are chosen to have a reasonable execution time. False alarm were 
+ never observed yet. Since random numbers are preserved through repetitions of
+ the simulations, the test should work for sure as long as the random number 
+ generation procedure of nest is not changed. If it is changed, failure of the
+ test is still very unlikely.
+ 
+ The intention of this script is to make sure that there are no gross errors in
+ the main functions of the ppd_sup_generator. It does not test the sinusoidal
+ hazard feature yet. 
+
+Remarks:
+
+  This test script was adapted from test_ppd_sup_generator.sli  
+
+
+Author:  June 2011, Deger
+SeeAlso: test_ppd_sup_generator.sli
+*/
+
+
+/unittest (6688) require
+/unittest using
+
+
+0.2 /err Set
+
+
+% 1) check for reasonable superposition spike rate
+% 2) check if superposition cv agrees with theory
+
+/check_sup_rate_and_cv
+{    
+   % test parameters
+    5 /gamma_shape Set
+    10.0 /rate Set
+    10000.0 /T Set
+    5 /n_proc Set
+    1. /h Set
+
+    ResetKernel
+    
+    << 
+      /resolution  h
+    >> /kernelparams Set
+    
+    0 kernelparams SetStatus 
+    
+    /gamma_sup_generator Create /psg Set
+
+    <<
+      /rate  rate
+      /gamma_shape gamma_shape
+      /n_proc n_proc
+    >> /params Set
+    
+    psg params SetStatus
+    
+    /spike_detector Create /sd Set
+
+    psg sd Connect
+    T Simulate
+    
+    sd GetStatus /events get /times get /spikes Set
+
+    % rate_sim = size(spikes) / (T*1e-3)
+    spikes cva size T 1e-3 mul div /rate_sim Set /spikes_array Set
+
+    % rate_ana = 1./(1./lam + d*1e-3)
+    rate n_proc mul /rate_ana Set
+    
+    % ratio = rate_sim / rate_ana
+    rate_sim rate_ana div /ratio Set
+    
+    % this could fail due to bad luck. However, if it passes once, then it should
+    % always do so, since the random numbers are reproducible in NEST.
+    1.0 err sub ratio lt
+    ratio 1.0 err add lt
+    and assert_or_die
+    
+    %isi = []
+    %for i in xrange(1,len(spikes)):
+    %    isi.append(spikes[i]-spikes[i-1])
+    0.0 /t Set
+    spikes cva { dup t sub exch /t Set  } Map /isi Set
+
+
+    %# compute moments of ISI to get mean and variance
+    %isi_m1 = 0. 
+    %isi_m2 = 0.
+    %for t in isi:
+    %    isi_m1 += t
+    %    isi_m2 += t**2
+    0. /isi_m1 Set
+    0. /isi_m2 Set
+    isi { dup isi_m1 add /isi_m1 Set dup mul isi_m2 add /isi_m2 Set} forall
+    
+    %isi_mean = isi_m1 / len(isi)
+    %isi_var = isi_m2/ len(isi) - isi_mean**2
+    %cvsq = isi_var/isi_mean**2
+    isi_m1 isi size exch pop div /isi_mean Set
+    isi_m2 isi size exch pop div isi_mean dup mul sub /isi_var Set
+    isi_var isi_mean 2 pow  div /cvsq_sim Set
+
+    %theoretical CV**2 for PPD, should match gamma also, see Deger et al 2011, JCNS
+    1.0 1.0 gamma_shape sqrt div sub /dbar Set
+    1.0 rate div 1e3 mul /mu Set
+    mu dbar mul /dead_time Set
+    
+    1.0 1.0 n_proc add div /cvfact1 Set
+    n_proc 1.0 sub 2.0 1.0 dbar sub n_proc 1 add pow mul add /cvfact2 Set
+    cvfact1 cvfact2 mul /cvsq_theo Set
+
+    cvsq_sim cvsq_theo div /ratio_cvsq Set 
+    1.0 err sub ratio_cvsq leq
+    ratio_cvsq 1.0 err add leq
+    and assert_or_die
+    
+} def
+
+
+
+%3) check if single process respect isi moments
+
+/check_single_rate_and_isi
+{    
+   % test parameters
+    7 /gamma_shape Set
+    15.0 /rate Set
+    100000.0 /T Set
+    1 /n_proc Set
+    1.0 /h Set
+
+    ResetKernel
+    
+    << 
+      /resolution  h
+    >> /kernelparams Set
+    
+    0 kernelparams SetStatus 
+    
+    /gamma_sup_generator Create /psg Set
+
+    <<
+      /rate  rate
+      /gamma_shape gamma_shape
+      /n_proc n_proc
+    >> /params Set
+    
+    psg params SetStatus
+    
+    /spike_detector Create /sd Set
+    
+    psg sd Connect
+    T Simulate
+    
+    sd GetStatus /events get /times get /spikes Set
+
+    % rate_sim = size(spikes) / (T*1e-3)
+    spikes cva size T 1e-3 mul div /rate_sim Set /spikes_array Set
+
+    % rate_ana = 1./(1./lam + d*1e-3)
+    rate n_proc mul /rate_ana Set
+    
+    % ratio = rate_sim / rate_ana
+    rate_sim rate_ana div /ratio Set
+    
+    % this could fail due to bad luck. However, if it passes once, then it should
+    % always do so, since the random numbers are reproducible in NEST.
+    1.0 err sub ratio lt
+    ratio 1.0 err add lt
+    and assert_or_die
+    
+    %isi = []
+    %for i in xrange(1,len(spikes)):
+    %    isi.append(spikes[i]-spikes[i-1])
+    % Assert that min(isi)>=d
+    0.0 /t Set
+    spikes cva { dup t sub exch /t Set  } Map /isi Set
+%    isi Rest Min dead_time geq assert_or_die
+
+    %# compute moments of ISI to get mean and variance
+    %isi_m1 = 0. 
+    %isi_m2 = 0.
+    %for t in isi:
+    %    isi_m1 += t
+    %    isi_m2 += t**2
+    0. /isi_m1 Set
+    0. /isi_m2 Set
+    isi { dup isi_m1 add /isi_m1 Set dup mul isi_m2 add /isi_m2 Set} forall
+    
+    %isi_mean = isi_m1 / len(isi)
+    %isi_var = isi_m2/ len(isi) - isi_mean**2
+    %cvsq = isi_var/isi_mean**2
+    isi_m1 isi size exch pop div /isi_mean Set
+    isi_m2 isi size exch pop div isi_mean dup mul sub /isi_var Set
+    isi_var isi_mean 2 pow div /cvsq_sim Set
+
+    %theoretical CV**2, see Deger et al 2011, JCNS
+    % here we first match the equivalent PPD and then compute its CV**2
+    % this is done because formulas are simpler to remember and code can
+    % be reused.
+    1.0 1.0 gamma_shape div sqrt sub /dbar Set
+    1.0 rate div 1e3 mul /mu Set
+    mu dbar mul /dead_time Set
+
+    1.0 rate div /mu Set
+    mu dead_time sub /lam Set
+    1.0 lam div mu div 2 pow /cvsq_theo Set
+    
+    cvsq_sim cvsq_theo div /ratio_cvsq Set 
+    1.0 err sub ratio_cvsq leq
+    ratio_cvsq 1.0 err add leq
+    and assert_or_die
+    
+} def
+
+
+/check_different_outputs
+   {
+
+   % test parameters
+    2 /gamma_shape Set
+    25.0 /rate Set
+    10.0 /T Set
+    1000 /n_proc Set
+    0.01 /h Set
+
+    ResetKernel
+    
+    << 
+      /resolution  h
+    >> /kernelparams Set
+    
+    0 kernelparams SetStatus 
+    
+    /gamma_sup_generator Create /psg Set
+
+    <<
+      /rate  rate
+      /gamma_shape gamma_shape
+      /n_proc n_proc
+    >> /params Set
+    
+    psg params SetStatus
+    
+    /spike_detector Create /sd1 Set
+    /spike_detector Create /sd2 Set
+
+    psg sd1 Connect
+    psg sd2 Connect
+    T Simulate
+
+    %first we check if the spike trains are different
+    [sd1 sd2] {[/events /times] get} forall  neq  % spike trains differ
+    assert_or_die
+    
+    %and we also check the rates since we simulated anyway
+    sd1 GetStatus /events get /times get /spikes1 Set
+    sd2 GetStatus /events get /times get /spikes2 Set
+    
+    % rate_sim = size(spikes) / (T*1e-3)
+    spikes1 cva size T 1e-3 mul div /rate_sim1 Set /spikes_array1 Set
+    spikes2 cva size T 1e-3 mul div /rate_sim2 Set /spikes_array2 Set
+
+    % rate_ana = 1./(1./lam + d*1e-3)
+    rate n_proc mul /rate_ana Set
+    
+    % ratio = rate_sim / rate_ana
+    rate_sim1 rate_ana div /ratio1 Set
+    rate_sim2 rate_ana div /ratio2 Set
+    
+    % this could fail due to bad luck. However, if it passes once, then it should
+    % always do so, since the random numbers are reproducible in NEST.
+    1.0 err sub ratio1 lt
+    ratio1 1.0 err add lt
+    and assert_or_die
+    
+    1.0 err sub ratio2 lt
+    ratio2 1.0 err add lt
+    and assert_or_die
+} def
+
+
+
+check_sup_rate_and_cv
+check_single_rate_and_isi
+check_different_outputs
+

commit 0cc24db95152d5d8c6219a6087572645729de198

Fixed merge problems introduced in r9265
git-svn-id: https://www.nest-initiative.uni-freiburg.de/nest/branches/10kproject@9267 4fa6ec7b-3718-0410-b4d6-90ca9082ef84

nestkernel/scheduler.cpp
@@ -138,7 +138,6 @@ void nest::Scheduler::init_()
 	       "Error initializing condition variable ready_");
     throw PthreadException(status);
   }
-
 #else
 #ifndef _OPENMP
   if (n_threads_ > 1)
@@ -499,6 +498,84 @@ void nest::Scheduler::resume()
     serial_update();
 #endif
 #endif
+  }   
+  simulating_ = false;   
+  finalize_nodes();   
+   
+  if (print_time_)   
+    std::cout << std::endl;   
+   
+  Communicator::synchronize();   
+   
+  if(terminate_)   
+  {   
+    net_.message(SLIInterpreter::M_ERROR, "Scheduler::resume", "Exiting on error or user signal.");   
+    net_.message(SLIInterpreter::M_ERROR, "Scheduler::resume", "Scheduler: Use 'ResumeSimulation' to resume.");   
+       
+    if(SLIsignalflag != 0)   
+    {   
+      SystemSignal signal(SLIsignalflag);   
+      SLIsignalflag=0;   
+      throw signal;   
+    }   
+    else   
+      throw SimulationError();   
+       
+    return;   
+  }   
+     
+  net_.message(SLIInterpreter::M_INFO, "Scheduler::resume", "Simulation finished.");   
+}   
+
+/**   
+ * the single-threaded update is just a single loop over all nodes   
+ * per time-slice.
+ */   
+void nest::Scheduler::serial_update()   
+{   
+  std::vector<Node*>::iterator i;   
+   
+  do   
+  {   
+    if (print_time_)   
+      gettimeofday(&t_slice_begin_, NULL);   
+   
+    if ( from_step_ == 0 )  // deliver only at beginning of slice   
+    {   
+      deliver_events_(0);   
+#ifdef HAVE_MUSIC   
+      // advance the time of music by one step (min_delay * h) must   
+      // be done after deliver_events_() since it calls   
+      // music_event_out_proxy::handle(), which hands the spikes over to   
+      // MUSIC *before* MUSIC time is advanced   
+      if (slice_ > 0)   
+        Communicator::advance_music_time(1);   
+   
+      net_.update_music_event_handlers_(clock_, from_step_, to_step_);   
+#endif   
+    }   
+   
+    for (i = nodes_vec_[0].begin(); i != nodes_vec_[0].end(); ++i)   
+      update_(*i);   
+         
+    if ( static_cast<ulong_t>(to_step_) == min_delay_ ) // gather only at end of slice    
+      gather_events_();   
+   
+    advance_time_();   
+   
+    if(SLIsignalflag != 0)   
+    {   
+      net_.message(SLIInterpreter::M_INFO, "Scheduler::serial_update", "Simulation exiting on user signal.");   
+      terminate_=true;   
+    }   
+   
+    if (print_time_)   
+    {    
+      gettimeofday(&t_slice_end_, NULL);   
+      print_progress_();   
+    }   
+  } while((to_do_ != 0) && (! terminate_));   
+}
 
 void nest::Scheduler::threaded_update_openmp()
 {