5d806aae0b34ce4b523e0cd1497b2cdf9df84f09
plesser <plesser@4fa6ec7b-3718-0410-b4d6-90ca9082ef84>
2012-03-07 05:56:39 +0000

    Protected all dictstack cache accesses with .at().
    git-svn-id: https://www.nest-initiative.uni-freiburg.de/nest/branches/10kproject@9636 4fa6ec7b-3718-0410-b4d6-90ca9082ef84

--- sli/dictstack.h
+++ sli/dictstack.h
@@ -17,33 +17,32 @@
 #ifndef DICTIONARYSTACK_H
 #define DICTIONARYSTACK_H
 /* 
-    SLI's dictionary stack
-*/
+ SLI's dictionary stack
+ */
 #include <typeinfo>
 #include "dictdatum.h"
 #include <list>
 #include "sliexceptions.h"
 
-
 /***************************************************************
 
-Problems:
-     
-- is ist better to uses dictionaries as references to common
-  objects like in PS. What is the exact meaning of undef and
-  where in our current situation (read RedBook).     
-- more efficient implementation exploiting 
-  the name ids (documented elsewhere).
+ Problems:
+
+ - is ist better to uses dictionaries as references to common
+ objects like in PS. What is the exact meaning of undef and
+ where in our current situation (read RedBook).
+ - more efficient implementation exploiting
+ the name ids (documented elsewhere).
 
 
 
-    History:
-            (1) using list<Dictionary> 
-               MD, 23.6.1, Freiburg
-            (0) first version (single dictionary)
-               MOG, MD, June 1997, Freiburg
-***************************************************************
-*/
+ History:
+ (1) using list<Dictionary>
+ MD, 23.6.1, Freiburg
+ (0) first version (single dictionary)
+ MOG, MD, June 1997, Freiburg
+ ***************************************************************
+ */
 
 /**
  * The macro DICTSTACK_CACHE switches on two caches:
@@ -54,7 +53,7 @@ Problems:
  * If a name is looked up, it is looked up in the cache. 
  * If the cache does not have an entry, the dictionary stack is searched and
  * the name/token combination is added to the cache. 
- */ 
+ */
 #ifndef DICTSTACK_CACHE
 #define DICTSTACK_CACHE 1
 #endif
@@ -64,12 +63,12 @@ Problems:
 class DictionaryStack
 {
 private:
-    const Token VoidToken;
-    std::list<DictionaryDatum> d;
-    DictionaryDatum base_;
+  const Token VoidToken;
+  std::list<DictionaryDatum> d;
+  DictionaryDatum base_;
 #ifdef DICTSTACK_CACHE
-    std::vector<const Token *> cache_;
-    std::vector<const Token *> basecache_;
+  std::vector<const Token *> cache_;
+  std::vector<const Token *> basecache_;
 #endif
 
 public:
@@ -77,25 +76,24 @@ public:
   DictionaryStack(const DictionaryStack&);
   ~DictionaryStack();
 
-    
 #ifdef DICTSTACK_CACHE
   /**
    * Add a token to the cache.
    */
-  void cache_token( const Name &n, const Token *result)
+  void cache_token(const Name &n, const Token *result)
   {
-    Name::handle_t key=n.toIndex();
-    if (key>=cache_.size())
-      cache_.resize(Name::num_handles()+100,0);
-    cache_[key]= result;
+    Name::handle_t key = n.toIndex();
+    if (key >= cache_.size())
+      cache_.resize(Name::num_handles() + 100, 0);
+    cache_.at(key) = result;
   }
 
-  void basecache_token( const Name &n, const Token *result)
+  void basecache_token(const Name &n, const Token *result)
   {
-    Name::handle_t key=n.toIndex();
-    if (key>=basecache_.size())
-      basecache_.resize(Name::num_handles()+100,0);
-    basecache_[key]= result;
+    Name::handle_t key = n.toIndex();
+    if (key >= basecache_.size())
+      basecache_.resize(Name::num_handles() + 100, 0);
+    basecache_.at(key) = result;
   }
 
   /**
@@ -104,24 +102,23 @@ public:
    */
   void clear_token_from_cache(const Name&n)
   {
-    Name::handle_t key=n.toIndex();
-    if(key<cache_.size())
-      cache_[key]=0;
+    Name::handle_t key = n.toIndex();
+    if (key < cache_.size())
+      cache_.at(key) = 0;
   }
 
   void clear_token_from_basecache(const Name&n)
   {
-    Name::handle_t key=n.toIndex();
-    if(key<basecache_.size())
-      basecache_[key]=0;
+    Name::handle_t key = n.toIndex();
+    if (key < basecache_.size())
+      basecache_.at(key) = 0;
   }
 
   void clear_dict_from_cache(DictionaryDatum d)
   {
-    for(TokenMap::iterator i = d->begin(); i != d->end(); ++i)
-	cache_[i->first.toIndex()]=0;
+    for (TokenMap::iterator i = d->begin(); i != d->end(); ++i)
+      cache_.at(i->first.toIndex()) = 0;
   }
-
 
   /** Clear the entire cache.
    * This should be  called whenever a dictionary is pushed or poped.
@@ -129,137 +126,135 @@ public:
    */
   void clear_cache()
   {
-    const size_t cache_size=cache_.size();
+    const size_t cache_size = cache_.size();
 
-    for(size_t i=0; i< cache_size; ++i)
-      cache_[i]=0;
+    for (size_t i = 0; i < cache_size; ++i)
+      cache_.at(i) = 0;
   }
 
 #endif
 
   const Token & lookup(const Name &n)
-  {  
+  {
     try
-      {
-	return lookup2(n);
-      }
-    catch(UndefinedName)
+    {
+      return lookup2(n);
+    } catch (UndefinedName)
     {
       return VoidToken;
     }
   }
 
   const Token & lookup2(const Name &n)
-  { 
+  {
 #ifdef DICTSTACK_CACHE
-    Name::handle_t key=n.toIndex();
-    if (key<cache_.size())
-      {
-    	const Token *result=cache_[key];
-    	if(result)
-    	  return *result;
-      }
+    Name::handle_t key = n.toIndex();
+    if (key < cache_.size())
+    {
+      const Token *result = cache_.at(key);
+      if (result)
+        return *result;
+    }
 #endif
 
-    std::list<DictionaryDatum>::const_iterator i=d.begin();
-    
-    while (i!=d.end())
+    std::list<DictionaryDatum>::const_iterator i = d.begin();
+
+    while (i != d.end())
+    {
+      TokenMap::const_iterator where = (*i)->find(n);
+      if (where != (*i)->end())
       {
-	TokenMap::const_iterator where =(*i)->find(n);
-	if(where!=(*i)->end())
-	  {
 #ifdef DICTSTACK_CACHE
-	    cache_token(n,&(where->second)); // Update the cache 
+        cache_token(n, &(where->second)); // Update the cache
 #endif
-	    return where->second;
-	  }
-	++i;
+        return where->second;
       }
+      ++i;
+    }
     throw UndefinedName(n.toString());
   }
-  
+
   /** Lookup a name searching only the bottom level dictionary.
    *  If the Name is not found,
    *  @a VoidToken is returned.
    */
   const Token & baselookup(const Name &n) // lookup in a specified
-  {                                           // base dictionary
+  { // base dictionary
 #ifdef DICTSTACK_CACHE
-      Name::handle_t key=n.toIndex();
-      if (key<basecache_.size())
-	{
-	  const Token *result=basecache_[key];
-	  if(result)
-	    return *result;
-	}
-#endif
-      TokenMap::const_iterator where =base_->find(n);
-#ifdef DICTSTACK_CACHE
-      cache_token(n,&(where->second));// Update the cache 
-      basecache_token(n,&(where->second)); // and the basecache 
-#endif
-      return where->second;
+    Name::handle_t key = n.toIndex();
+    if (key < basecache_.size())
+    {
+      const Token *result = basecache_.at(key);
+      if (result)
+        return *result;
     }
+#endif
+    TokenMap::const_iterator where = base_->find(n);
+#ifdef DICTSTACK_CACHE
+    cache_token(n, &(where->second)); // Update the cache
+    basecache_token(n, &(where->second)); // and the basecache
+#endif
+    return where->second;
+  }
 
   /** Test for a name searching all dictionaries on the stack.
    */
   bool known(const Name &n)
-    { 
+  {
 #ifdef DICTSTACK_CACHE
-      Name::handle_t key=n.toIndex();
-      if (key<cache_.size())
-	{
-	  const Token *result=cache_[key];
-	  if(result)
-	    return true;
-	}
-#endif
-      std::list<DictionaryDatum>::const_iterator i(d.begin());
-
-      while (i!=d.end())
-	{
-	  TokenMap::const_iterator where =(*i)->find(n);
-	  if(where!=(*i)->end())
-	    {
-#ifdef DICTSTACK_CACHE
-	      cache_token(n,&(where->second)); // Update the cache 
-#endif
-	      return true;
-	  }
-	++i;
-      }
-      return false;
+    Name::handle_t key = n.toIndex();
+    if (key < cache_.size())
+    {
+      const Token *result = cache_.at(key);
+      if (result)
+        return true;
     }
-  
+#endif
+    std::list<DictionaryDatum>::const_iterator i(d.begin());
+
+    while (i != d.end())
+    {
+      TokenMap::const_iterator where = (*i)->find(n);
+      if (where != (*i)->end())
+      {
+#ifdef DICTSTACK_CACHE
+        cache_token(n, &(where->second)); // Update the cache
+#endif
+        return true;
+      }
+      ++i;
+    }
+    return false;
+  }
+
   /** Test for a name in the bottom level dictionary.
    */
   bool baseknown(const Name &n) // lookup in a specified
-  {                                                  // base dictionary
+  { // base dictionary
 #ifdef DICTSTACK_CACHE
-      Name::handle_t key=n.toIndex();
-      if (key<basecache_.size())
-	{
-	  const Token *result=basecache_[key];
-	  if(result)
-	    return true;
-	}
+    Name::handle_t key = n.toIndex();
+    if (key < basecache_.size())
+    {
+      const Token *result = basecache_.at(key);
+      if (result)
+        return true;
+    }
 #endif
-      TokenMap::const_iterator where =base_->find(n);
-      if(where!=base_->end())
-	{
+    TokenMap::const_iterator where = base_->find(n);
+    if (where != base_->end())
+    {
 #ifdef DICTSTACK_CACHE
-	  basecache_token(n,&(where->second)); // Update the basecache 
-	  cache_token(n,&(where->second)); // and the cache 
+      basecache_token(n, &(where->second)); // Update the basecache
+      cache_token(n, &(where->second)); // and the cache
 #endif
-	  return true;
-	}
-      return false;
+      return true;
+    }
+    return false;
   }
 
-
-   //
-   // def and def_move operate on the top level dictionary.
-   // undef is not defined for the dictionary stack.
+  //
+  // def and def_move operate on the top level dictionary.
+  // undef is not defined for the dictionary stack.
 
   /** Bind a Token to a Name in the top level dictionary.
    *  The Token is copied.
@@ -279,7 +274,7 @@ public:
    *  The Token is moved.
    */
   void def_move(const Name&, Token &);
-  
+
   /**
    * This function must be called once to initialize the systemdict cache.
    */
@@ -291,39 +286,37 @@ public:
   void basedef_move(const Name &n, Token &t);
 
   bool where(const Name&, Token&);
-    
+
   void pop(void);
 
+  //
+  // a Dictionary is always wrapped in a Token
+  //
+  void top(Token &) const;
+  void push(const DictionaryDatum &);
+  void push(Token &);
 
-   //
-   // a Dictionary is always wrapped in a Token
-   //
-   void top(Token &) const;
-   void push(const DictionaryDatum &);
-   void push(Token &);
-   
-   void clear(void);
-   void toArray(TokenArray &) const;
-   //
-   // move is efficient for interaction with operand and execution
-   // stack, but can not be implemented in this version
-   //
+  void clear(void);
+  void toArray(TokenArray &) const;
+  //
+  // move is efficient for interaction with operand and execution
+  // stack, but can not be implemented in this version
+  //
 //    void pop_move(Token &) const;
 //    void push_move(const Token &);
 
-    // 
-    // number of dictionaries currently on the stack
-    //
+  //
+  // number of dictionaries currently on the stack
+  //
   size_t size(void) const;
 
-
-    //
-    // info for debugging purposes. 
-    // calls info(ostream&) for all dictionaries 
-    //
+  //
+  // info for debugging purposes.
+  // calls info(ostream&) for all dictionaries
+  //
   void info(std::ostream&) const;
   void top_info(std::ostream &) const; // calls info of top dictionary
-  const DictionaryStack& operator=(const DictionaryStack&); 
+  const DictionaryStack& operator=(const DictionaryStack&);
 };
 
 inline
@@ -335,10 +328,10 @@ void DictionaryStack::def(const Name &n, const Token &t)
   // VoidToken is an illegal value for t.
   //
 #ifdef DICTSTACK_CACHE
-  cache_token(n,&((*d.begin())->insert(n,t)));
+  cache_token(n, &((*d.begin())->insert(n, t)));
 #endif
 #ifndef DICTSTACK_CACHE
-        (**d.begin())[n]=t;
+  (**d.begin())[n]=t;
 #endif
 }
 
@@ -351,19 +344,15 @@ void DictionaryStack::def_move(const Name &n, Token &t)
   // VoidToken is an illegal value for t.
   // def_move returns t as the VoidToken.  
   //
-    /* clear_token_from_cache(n); */
+  /* clear_token_from_cache(n); */
 
 #ifdef DICTSTACK_CACHE
-     cache_token(n,&((*d.begin())->insert_move(n,t))); 
+  cache_token(n, &((*d.begin())->insert_move(n, t)));
 #endif
 #ifndef DICTSTACK_CACHE
-     (*d.begin())->insert_move(n,t);
+  (*d.begin())->insert_move(n,t);
 #endif
 }
 
 #endif
-
-
-
-
 
